30b2b7cdb4b9f80d867af183c4b8eaaaffd850a8
Lucas Bentolila <lucasbentolila17@gmail.com>
2025-11-06 02:46:17 -0300

    Dia 1: Hola mundo en Java

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+        System.out.println("Mi nombre es Lucas Bentolila");
+        System.out.println("Estoy comenzando mi camino como desarrollador Java!");
+    }
+}
